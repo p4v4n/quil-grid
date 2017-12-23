9a0c1d18d3350dc4a0c53f12d8e02ec1c61c2763
p4v4n <pavan.barcelona@gmail.com>
2017-12-23 20:16:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# quil-grid
+
+A Quil Grid designed to ... well, that part is up to you.
+
+## Usage
+
+Run `lein compile` command and open `index.html` in your browser.
+
+For interactive development run `lein cljsbuild auto` command. This command will be recompiling cljs to js each time you modify `core.cljs` and you can see result immediately by refreshing page.
+
+## License
+
+Copyright © 2017 
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/quil_grid/core.cljs
@@ -0,0 +1,69 @@
+(ns quil-grid.core
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]))
+
+(def m 100)          ;;no: of rows
+(def n 100)          ;;no: of columns
+(def width 800)
+(def height 600)
+
+(defn generate-random-color []
+  (rand-int 256))
+;;---------------------------
+(defn generate-single-rect [c x y i j]
+  {:color c
+   :loc {:x x :y y}
+   :index {:i i :j j}})
+
+(defn generate-single-row [y n i]
+  (mapv #(generate-single-rect 100
+                               (* (/  width n) %) y
+                               i  %)
+        (range n)))
+
+(defn generate-initial-state [m n]
+  (mapv #(generate-single-row (* (/ height m)  %) n % ) (range m)))
+;;------
+(defn setup []
+  (q/frame-rate 5)
+  (q/no-stroke)
+  (q/color-mode :hsb)
+  (generate-initial-state m n))
+;;-----------------------------------
+(defn update-color [color]
+  (mod (+ color (rand-int 10)) 256))
+
+(defn update-single-rect [s-state]
+  (update s-state :color update-color))
+
+(defn update-single-row [s-row]
+  (mapv update-single-rect s-row))
+
+(defn update-whole-grid [state]
+  (mapv update-single-row state))
+;;-------------------
+(defn update-state [state]
+  (update-whole-grid state))
+;;--------------------------------------------
+(defn draw-single-rect [s-state]
+  (q/fill (:color s-state) 255 255)
+  (q/rect (get-in s-state [:loc :x]) (get-in s-state [:loc :y]) (/ width n) (/ height m)))
+
+(defn draw-single-row [s-row]
+  (mapv draw-single-rect s-row))
+
+(defn draw-whole-grid [state]
+  (mapv draw-single-row state))
+
+;;----------
+(defn draw-state [state]
+  (q/background 240)
+  (draw-whole-grid state))
+;;-----------------------------------------
+(q/defsketch quil-grid
+  :host "quil-grid"
+  :size [width height]
+  :setup setup                ; setup function called only once, during sketch initialization.
+  :update update-state ; update-state is called on each iteration before draw-state.
+  :draw draw-state
+  :middleware [m/fun-mode])
